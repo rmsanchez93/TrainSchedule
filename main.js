cf4e7c3ec7db654712a2d86331c885befaf3b6e2
RMSanchez <raulsg93@gmail.com>
2018-06-14 18:14:48 -0500

    basic built finally broke and did Bootstrap

---
+++ main.js
@@ -0,0 +1,13 @@
+//test and get firebaseup and running plz
+
+var 
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDQzwqpPer0mE-YF5ySgW3M2zWK2cLCr7s",
+    authDomain: "trainapp-ebbfc.firebaseapp.com",
+    databaseURL: "https://trainapp-ebbfc.firebaseio.com",
+    projectId: "trainapp-ebbfc",
+    storageBucket: "trainapp-ebbfc.appspot.com",
+    messagingSenderId: "1071721238026"
+  };
+  firebase.initializeApp(config);
